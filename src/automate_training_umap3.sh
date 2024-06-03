adddfa764d8d7c1fe17e34b8c761d6a5a495af06
Reese Kneeland <reesekneeland@gmail.com>
2024-06-03 13:31:01 -0500

    Updating umap training code

--- src/automate_training_umap3.sh
+++ src/automate_training_umap3.sh
@@ -4,7 +4,7 @@ jupyter nbconvert Train_MindEye_UMAP.ipynb --to python
 metric=cosine
 min_dist=0.1
 dimensions=1024
-for n_neighbors in 3 25 250 1000 5000 10 100 500 2500; do
+for n_neighbors in 10000 3 25 250 1000 5000 10 100 500 2500; do
     python Train_MindEye_UMAP.py --data_path ../data/ \
                             --vd_cache_dir ../versatile_diffusion \
                             --model_name "subj01_hypatia_UMAP_upscaled_${dimensions}dim_${metric}_${n_neighbors}neighbors_${min_dist}dist" \
